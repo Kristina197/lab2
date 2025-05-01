59bd7267fc59a2d080d225725257a326c74b2dc9
Kristina197 <butkris06@gmail.com>
2025-05-02 00:15:30 +0700

    Update 3.cs

--- 3.cs
+++ 3.cs
@@ -2,63 +2,61 @@ using System;
 
 class Program
 {
+    // Функция для переворота числа и удаления ведущих нулей
+    static int revers_num(int num)
+    {
+        int revers = 0;
+        while (num != 0)  // Переворачиваем число
+        {
+            revers = revers * 10 + num % 10;
+            num /= 10;
+        }
+        return revers;
+    }
+
     static int Main()
     {
-        Console.OutputEncoding = System.Text.Encoding.UTF8; //Добавление русского языка
-        int size;
-        Console.Write("Введите количество элементов массива: ");
-        size = int.Parse(Console.ReadLine());
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        int n;
+        string input = Console.ReadLine();
+        if (!int.TryParse(input, out n))
+        {
+            Console.WriteLine("Количество элементов должно быть положительным и целым");
+            return 1;
+        }
 
         // Проверка на корректность ввода
-        if (size <= 0)
+        if (n <= 0)
         {
-            Console.WriteLine("Количество элементов должно быть положительным");
-            return 1; // Завершаем с ошибкой
+            Console.WriteLine("Количество элементов должно быть положительным и целым");
+            return 1;
+        }
+        else
+        {
+            for (int i = 0; i < n; ++i)
+            {
+                input = Console.ReadLine();
+                int num;
+                if (!int.TryParse(input, out num))
+                {
+                    Console.WriteLine("Количество элементов должно быть положительным и целым");
+                    return 1;
+                }
+
+                // Проверка на корректность ввода
+                if (num <= 0)
+                {
+                    Console.WriteLine("Числа должны быть положительным и целым");
+                    return 1;
+                }
+                else
+                {
+                    // Переворачиваем число и выводим с пробелом
+                    Console.Write(revers_num(num) + " ");
+                }
+            }
         }
 
-        // Создание массива размера size
-        int[] nums = new int[size];
-        Console.WriteLine("Введите элементы массива:");
-
-        // Ввод элементов
-        for (int j = 0; j < size; j++)
-        {
-            int num = int.Parse(Console.ReadLine());
-
-            // Проверка каждого элемента
-            if (num <= 0)
-            {
-                Console.WriteLine("Элементы должны быть положительными и целыми");
-                return 1; // В C# память освобождается автоматически
-            }
-            else
-            {
-                nums[j] = num;
-            }
-        }
-
-        // Обработка и вывод чисел
-        for (int i = 0; i < size; i++)
-        {
-            if (nums[i] < 10)
-            {
-                continue; // Пропускаем числа меньше 10
-            }
-
-            int rez = 0;
-            int original_num = nums[i]; // Сохраняем оригинальное число
-
-            while (original_num > 0)
-            {
-                rez = rez * 10 + original_num % 10;
-                original_num /= 10;
-            }
-
-            // Удаление ведущих нулей происходит автоматически при выводе числа
-            Console.Write(rez + " ");
-        }
-
-        Console.WriteLine();
         return 0;
     }
 }
