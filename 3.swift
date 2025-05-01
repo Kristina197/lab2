33eda252761bc6173092cc1eab037c5b6a0c346a
Kristina197 <butkris06@gmail.com>
2025-05-02 00:42:40 +0700

    Update 3.swift

--- 3.swift
+++ 3.swift
@@ -1,50 +1,50 @@
 import Foundation
-print("Введите количество элементов массива: ", terminator: "")
-guard let sizeInput = readLine(), let size = Int(sizeInput) else {
-    print("Ошибка ввода")
-    exit(1)
-}
 
-// Проверка на корректность ввода
-if size <= 0 {
-    print("Количество элементов должно быть положительным")
-    exit(1) // Завершаем с ошибкой
-}
-
-// Создание массива размера size (аналог динамического массива в C++)
-var nums = [Int](repeating: 0, count: size)
-print("Введите элементы массива:")
-
-// Ввод элементов
-for j in 0..<size {
-    guard let numInput = readLine(), let num = Int(numInput) else {
-        print("Ошибка ввода")
-        exit(1)
+// Функция для переворота числа и удаления ведущих нулей
+func revers_num(_ num: Int) -> Int {
+    var num = num
+    var revers = 0
+    while num != 0 {  // Переворачиваем число
+        revers = revers * 10 + num % 10
+        num /= 10
     }
-        
-// Проверка каждого элемента
-    if num <= 0 {
-        print("Элементы должны быть положительными и целыми")
-        exit(1) // В Swift память освобождается автоматически
+    return revers
+}
+
+func main() -> Int {
+    setlocale(LC_ALL, "")
+    
+    guard let n = Int(readLine() ?? "") else {
+        print("Количество элементов должно быть положительным и целым")
+        return 1
+    }
+    
+    // Проверка на корректность ввода
+    if n <= 0 {
+        print("Количество элементов должно быть положительным и целым")
+        return 1
     } 
     else {
-        nums[j] = num
+        for _ in 0..<n {
+            guard let num = Int(readLine() ?? "") else {
+                print("Числа должны быть положительным и целым")
+                return 1
+            }
+            
+            // Проверка на корректность ввода
+            if num <= 0 {
+                print("Числа должны быть положительным и целым")
+                return 1
+            }
+            else {
+                // Вызываем функцию revers_num и выводим
+                print(revers_num(num), terminator: " ")
+            }
+        }
     }
+    
+    return 0
 }
 
-// Обработка и вывод чисел
-for i in 0..<size {
-    if nums[i] < 10 {
-        continue // Пропускаем числа меньше 10
-    }
-    var rez = 0
-    var originalNum = nums[i] // Сохраняем оригинальное число
-        
-    while originalNum > 0 {
-        rez = rez * 10 + originalNum % 10
-        originalNum /= 10
-    }
-        
-    // Удаление ведущих нулей происходит автоматически при выводе числа
-    print("\(rez) ", terminator: "")
-}
+// Вызываем основную функцию
+let _ = main()
